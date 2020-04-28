829c392ae46ad83d2ed6b177d5d5479195570dc2
Arun Shenoy <devarunshenoy99@gmail.com>
2020-04-28 15:15:01 +0530

    added ratings and review route

--- public/js/card.js
+++ public/js/card.js
@@ -1,27 +1,39 @@
+import { response } from "express"
+
 $('.card-header').hover(function () {
     $(this).css('cursor', 'pointer')
 })
 
 $('.card-header').click(function () {
-    let id = $(this).attr('id').replace('-header', '')
+    let id = $(this).attr('id').replace('header-', '')
     $(`#${id}`).collapse('toggle')
 })
 
-$('form').on('submit', function () {
-    let id = $(this).attr("id").replace('-form','')
-    let formdata = new FormData($(this))
-    fetch(`${window.location.protocol}//${window.location.hostname}:${window.location.port}/review/${id}`, {
+window.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const id = e.target.id.replace('form-', '')
+    const $form = document.querySelector(`#${e.target.id}`)
+    const $header = document.querySelector(`#header-${id}`)
+    const formData = new FormData($form)
+    let data = {}
+    formData.forEach((value, key) => {
+        data[key] = value
+    })
+    const url = `${window.location.protocol}//${window.location.hostname}:${window.location.port}/reviews/${id}`
+    fetch(url, {
         method: 'POST',
-        headers:{
+        headers: {
             'Content-Type': 'application/json'
-        }
+        },
+        body: JSON.stringify(data)
     })
     .then((response) => {
-        if (response.status == 201){
-            $(this).html('<p>Review submitted</p>')
+        if (response.status != 201) {
+            $header.classList.add('bg-danger')
+        } 
+        else {
+            $header.classList.add('bg-success')
+            $form.innerHTML = ""
         }
-    })
-    .catch((error) => {
-        console.log(error)
     })
 })
--- src/routers/reviews.js
+++ src/routers/reviews.js
@@ -1,6 +1,34 @@
 const express = require('express')
 const Review = require('../models/review')
+const Teacher = require('../models/teacher')
 
 const router = new express.Router()
+
+
+router.post('/reviews/:id', async (req, res) => {
+    const id = req.params.id
+    const reviewData = req.body
+    try
+    {
+        const teacher = Teacher.findById(id)
+        if (!teacher) {
+            return res.status(404).send()
+        }
+        if (reviewData.name === '') {
+            reviewData.name = "Anon"
+        }
+        const saveableReview = {
+            name: reviewData.name,
+            text: reviewData.review,
+            rating: parseInt(reviewData.ratings),
+            teacher: id
+        }
+        const review = new Review(saveableReview)
+        await review.save()
+        res.status(201).send()
+    } catch (e) {
+        res.status(400).send()
+    }
+})
 
 module.exports = router
