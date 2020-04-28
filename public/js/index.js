460234a8c60cb293ac3277e8d6a41cc280db7045
Arun Shenoy <devarunshenoy99@gmail.com>
2020-04-28 21:23:51 +0530

    carousel

--- public/js/index.js
+++ public/js/index.js
@@ -5,3 +5,21 @@ $('.dropdown-menu a').click(function () {
     console.log(window.location.port)
     window.location.href = `${window.location.protocol}//${window.location.hostname}:${window.location.port}/departments/${department}/teachers`
 })
+
+
+$(document).ready(function () {
+    $('#indexcarousel').carousel({ interval: 2000 })
+
+    $('#carouselbutton').click(function () {
+        if ($('#carouselbutton').children('span').hasClass('fa-pause')) {
+            $('#indexcarousel').carousel('pause')
+            $('#carouselbutton').children('span').removeClass('fa-pause')
+            $('#carouselbutton').children('span').addClass('fa-play')
+        }
+        else {
+            $('#indexcarousel').carousel('cycle')
+            $('#carouselbutton').children('span').removeClass('fa-play')
+            $('#carouselbutton').children('span').addClass('fa-pause')
+        }
+    })
+})
